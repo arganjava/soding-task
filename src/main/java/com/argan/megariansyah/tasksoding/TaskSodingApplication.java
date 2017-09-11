fe5a1d2c3e38fb9385d6bf696da1926138b9a084
arganjava@gmail.com <arganjava@gmail.com>
2017-09-11 13:13:47 +0700

    task APP init

---
+++ src/main/java/com/argan/megariansyah/tasksoding/TaskSodingApplication.java
@@ -0,0 +1,12 @@
+package com.argan.megariansyah.tasksoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSodingApplication.class, args);
+	}
+}
